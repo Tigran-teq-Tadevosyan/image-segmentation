80097e7cf972785cdb8a6ca2fdd71ae51022f710
Tigran-teq-Tadevosyan <tigran@tadevosyan.net>
2019-05-02 02:55:14 +0400

    added comments to the code

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from PIL import Image
 
-## We get the image and the load the pixels from it
-im = Image.open('./sample-images/jpeg-home.jpg') # Can be many different formats.
-pix = im.load()
-
 Mu = 20 ## we define the intensity with the 'mu' so if we set it higher it will only pick drastic changes 
 
 BLACK = (0,0,0)
@@ -12,50 +8,63 @@ GREEN = (0,255,0)
 RED = (255,0,0)
 BLUE = (0,0,255)
 
-## Getting my image sizes
-width = im.size[0]
-height = im.size[1]
-print("width",width)
-print("hight",height)
 
+"""
+prcessImage - colors the edges of the image and saves it to the 'result' folder
+    @ARGS
+        path (utf string) - the path of the original image [ default - path to sample image ]
+        Mu (int) - we define the intensity with the 'mu' so if we set it higher it will only pick drastic changes [ default - 20 the optimal value ]
+"""
+def prcessImage( path = './sample-images/jpeg-home.jpeg', Mu = 20 ):
 
-## Running of the image and changing the color of out picture to show predicted edges
-for x in range(1,width-1):
-    for y in range(1,height-1):
-        if pix[x,y][0] - pix[x,y+1][0] > Mu:
-            pix[x,y] = BLACK
-        elif pix[x,y][0] - pix[x,y+1][0] < -Mu:
-            pix[x,y] = GREEN
+    ## We get the image and the load the pixels from it
+    im = Image.open(path) # Can be many different formats.
+    pix = im.load()
 
-        if pix[x,y][1] - pix[x,y+1][1] > Mu:
-            pix[x,y] = BLACK
-        elif pix[x,y][1] - pix[x,y+1][1] < -Mu:
-            pix[x,y] = GREEN
+    ## Getting my image sizes
+    width = im.size[0]
+    height = im.size[1]
 
-        if pix[x,y][2] - pix[x,y+1][2] > Mu:
-            pix[x,y] = BLACK
-        elif pix[x,y][2] - pix[x,y+1][2] < -Mu:
-            pix[x,y] = GREEN
-
-for y in range(1,height-1):
+    ## Running of the image and changing the color of out picture to show predicted edges
     for x in range(1,width-1):
-        if pix[x,y][0] - pix[x+1,y][0] > Mu:
-            pix[x,y] = BLACK
-        elif pix[x,y][0] - pix[x+1,y][0] < -Mu:
-            pix[x,y] = GREEN
+        for y in range(1,height-1):
+            if (pix[x,y][0] - pix[x,y+1][0]) > Mu:
+                pix[x,y] = BLACK
+            elif pix[x,y][0] - pix[x,y+1][0] < -Mu:
+                pix[x,y] = GREEN
 
-        if pix[x,y][1] - pix[x+1,y][1] > Mu:
-            pix[x,y] = BLACK
-        elif pix[x,y][1] - pix[x+1,y][1] < -Mu:
-            pix[x,y] = GREEN
+            if pix[x,y][1] - pix[x,y+1][1] > Mu:
+                pix[x,y] = BLACK
+            elif pix[x,y][1] - pix[x,y+1][1] < -Mu:
+                pix[x,y] = GREEN
 
-        if pix[x,y][2] - pix[x+1,y][2] > Mu:
-            pix[x,y] = BLACK
-        elif pix[x,y][2] - pix[x+1,y][2] < -Mu:
-            pix[x,y] = GREEN
+            if pix[x,y][2] - pix[x,y+1][2] > Mu:
+                pix[x,y] = BLACK
+            elif pix[x,y][2] - pix[x,y+1][2] < -Mu:
+                pix[x,y] = GREEN
 
-im.save('./result/alive_parrot.png')  # Save the modified pixels as .png
+    for y in range(1,height-1):
+        for x in range(1,width-1):
+            if pix[x,y][0] - pix[x+1,y][0] > Mu:
+                pix[x,y] = BLACK
+            elif pix[x,y][0] - pix[x+1,y][0] < -Mu:
+                pix[x,y] = GREEN
 
+            if pix[x,y][1] - pix[x+1,y][1] > Mu:
+                pix[x,y] = BLACK
+            elif pix[x,y][1] - pix[x+1,y][1] < -Mu:
+                pix[x,y] = GREEN
+
+            if pix[x,y][2] - pix[x+1,y][2] > Mu:
+                pix[x,y] = BLACK
+            elif pix[x,y][2] - pix[x+1,y][2] < -Mu:
+                pix[x,y] = GREEN
+
+    im.save('./result/result.jpg')  # Save the modified pixels as .png
+
+
+
+'''
 exit() #Exit the program so the second method is not effecting
 
 count = 0
@@ -111,3 +120,4 @@ for pix_cord in changeArrey:
     pix[x_cord,y_cord] = BLACK
 
 im.save('./result/alive_parrot.png')  # Save the modified pixels as .png
+'''
--- start.py
+++ start.py
@@ -3,17 +3,37 @@ from PyQt5.QtWidgets import QMainWindow, QFileDialog, QDialog, QInputDialog, QWi
 from PyQt5.QtCore import pyqtSignal, pyqtSlot, QDir, QObject, Qt, QThread, QTimer, QEvent
 from PyQt5.QtGui import QPixmap, QPainter, QPen, QImage, QGuiApplication, QCursor, QClipboard
 
-import window_ui
+import sys
 
-class Window(window_ui):
+import window_ui
+from main import prcessImage
+
+class Window(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self.ui = window_ui()
+        self.ui = window_ui.Ui_MainWindow()
         self.ui.setupUi(self)
+        self.__selectImagePath = None
+
+    @pyqtSlot()
+    def selectImage(self):
+        options = QFileDialog.Options()
+        fileName, _ = QFileDialog.getOpenFileName(self,"Select Image", "","Select Image (*.jpg *jpeg)", options=options)
+        self.__selectImagePath = fileName
+        self.ui.ImagePath.setText(fileName)
+    
+    @pyqtSlot()
+    def process(self):
+        prcessImage(self.__selectImagePath,self.ui.MuSpinBox.value())
+
+        pixmap = QPixmap('./result/result.jpg')
+        self.ui.ImageDisplayLabel.setPixmap(pixmap)
+
+        # Optional, resize window to image size
+        self.resize(pixmap.width(),pixmap.height())
 
 if __name__ == '__main__':
-    global app, translator, window
     app = QApplication(sys.argv)
     window = Window()
     window.show()
